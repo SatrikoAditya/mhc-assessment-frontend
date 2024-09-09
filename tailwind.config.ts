1a24c749d9682e12e59f23a79fad344a69991e64
Satriko Aditya <65443335+SatrikoAditya@users.noreply.github.com>
2024-09-10 01:03:36 +0700

    #refactor remove unused color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "tailwindcss";
 
 const colors = {
-  "primary-text": "#f0f0f0",
   "primary-blue": "#4597f7",
   white: "#fefefe",
   "light-grey": "#a6a6a6",
@@ -17,14 +16,9 @@ const colors = {
   "light-sea-green": "#CBE7CF",
   gold: "#cc9200",
   eminence: "#7a3a92",
-  "persian-red": "#ce3a30",
-  "cocoa-brown": "#dc6803",
-  "border-color": "#e8f4fe",
   "grey-text": "#667085",
   red: "#d92c20",
   "amber-orange": "#F7A545",
-  "mid-grey": "#808080",
-  "neutral-150": "rgb(237 237 237)",
 };
 
 const config: Config = {
